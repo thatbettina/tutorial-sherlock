27eab942003bd1cd9e741e1d82a8a76cee7a27e8
Bettina <bettina.shzu@gmail.com>
2014-03-19 21:01:33 +0100

    combined map and story and added carousel

---
+++ map.js
@@ -0,0 +1,21 @@
+var geojson = [
+  { "geometry": { "type": "Point", "coordinates": [9.925201, 78.119775] },
+    "properties": { "title": "Madurai" } },
+  { "geometry": { "type": "Point", "coordinates": [1.0000000, 76.9666700] },
+    "properties": { "title": "Coimbatore" } },
+];
+
+var tiles = new L.StamenTileLayer("watercolor");
+
+var map = new L.Map("map", {
+    center: new L.LatLng(9.925201, 78.119775),
+    zoom: 5
+});
+map.addLayer(tiles);
+
+var markerMadurai = L.marker([9.925201, 78.119775],
+  {title:'Madurai'}).addTo(map);
+
+ 
+var markerCoimbatore = L.marker([11.0000000, 76.9666700],
+	{title:'Coimbatore'}).addTo(map);
---
+++ story.js
@@ -0,0 +1,38 @@
+// Array of story section elements.
+var sections = document.getElementsByTagName('section');
+
+// Helper to set the active section.
+var setActive = function(index, ease) {
+  _(sections).each(function(s) { s.className = s.className.replace(' active', '') });
+  sections[index].className += ' active';
+  return true;
+};
+
+// Bind to scroll events to find the active section.
+window.onscroll = _(function() {
+  // IE 8
+  if (window.pageYOffset === undefined) {
+    var y = document.documentElement.scrollTop;
+    var h = document.documentElement.clientHeight;
+  } else {
+    var y = window.pageYOffset;
+    var h = window.innerHeight;
+  }
+
+  // If scrolled to the very top of the page set the first section active.
+  if (y === 0) return setActive(0, true);
+
+  // Otherwise, conditionally determine the extent to which page must be
+  // scrolled for each section. The first section that matches the current
+  // scroll position wins and exits the loop early.
+  var memo = 0;
+  var buffer = (h * 0.3333);
+  var active = _(sections).any(function(el, index) {
+    memo += el.offsetHeight;
+    return y < (memo-buffer) ? setActive(index, true) : false;
+  });
+
+  // If no section was set active the user has scrolled past the last section.
+  // Set the last section active.
+  if (!active) setActive(sections.length - 1, true);
+}).debounce(10);
